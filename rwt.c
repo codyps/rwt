3c79f154a9633bb9dc689fbed3318052bf9842f8
John Mesmon <jmesmon@gmail.com>
2010-07-11 00:40:45 -0400

    impliment add_fn, copypasta del_fn: in progress.

--- rwt.c
+++ rwt.c
@@ -20,9 +20,101 @@ static void usage(char *name)
 	fprintf(stderr, usage_str, name);
 }
 
-int rm_fn(struct be_node *t, int argc, char **argv)
+int del_fn(struct be_node *t, int argc, char **argv)
 {
-	return -1;
+	if (argc < 1) {
+		FN_USE("need 1 tracker to add");
+		return -1;
+	}
+
+	char *tracker = argv[1];
+
+	fprintf(stderr, "DEL: '%s'\n", tracker);
+
+	struct be_str *tr = malloc(sizeof(*tr));
+	tr->len = strlen(tracker);
+	tr->data = tracker;
+
+	struct be_node *n_tr = malloc(sizeof(*n_tr));
+	n_tr->type = BE_STR;
+	n_tr->u.s = tr;
+
+	struct be_node *a = be_find(t, "announce", n_tr);
+	if (!a) {
+		free(tr);
+		free(n_tr);
+		fprintf(stderr, "ERR: %s\n", strerror(errno));
+		return -1;
+	} else if (a == n_tr) {
+		fprintf(stderr, "DEL: '%s' => 'announce'.\n", tracker);
+		return 2;
+	} else {
+		fprintf(stderr, "DEL: 'announce' already used.\n");
+		/* TODO: check if identical to the one we are adding? */
+
+	}
+
+	struct be_list *new_al = malloc(sizeof(*new_al));
+	new_al->len = 0;
+	new_al->nodes = 0;
+
+	struct be_node *new_al_n = malloc(sizeof(*new_al_n));
+	new_al_n->type = BE_LIST;
+	new_al_n->u.l = new_al;
+
+	struct be_node *al_n = be_find(t, "announce-list", new_al_n);
+	if (!al_n) {
+		fprintf(stderr, "ERR: %s\n", strerror(errno));
+		return -1;
+	} else if (al_n == new_al_n) {
+		fprintf(stderr, "DEL: created 'announce-list'.\n");
+	} else {
+		free(new_al);
+		free(new_al_n);
+		fprintf(stderr, "DEL: 'announce-list' already exsists.\n");
+	}
+
+	if (al_n->type != BE_LIST) {
+		fprintf(stderr, "WARN: 'announce-list' not a list.\n");
+		goto done_fail;
+	}
+
+	struct be_list *al = al_n->u.l;
+
+	size_t i;
+	for(i = 0; i < al->len; i++) {
+
+		struct be_node *ag_n = al->nodes[i];
+	
+		if (ag_n->type != BE_LIST) {
+			fprintf(stderr, "WARN: announce group %zu is not a list.\n", i);
+			continue;
+		}
+
+		struct be_list *ag = ag_n->u.l;
+
+		size_t ig;
+		for(ig = 0; ig < ag->len; i++) {
+			struct be_node *c_tr_n = ag->nodes[i];
+
+			if (c_tr_n != BE_STR) {
+				fprintf(stderr, "WARN: tracker %zu:%zu is not a str.\n", i, ig);
+				continue;
+			}
+
+			struct be_str *c_tr = c_tr_n->u.s;
+
+
+			/* TODO: operate on c_tr */
+			if (!be_str_cmp(c_tr, tr)) {
+				//
+			}
+		}
+	}
+
+done_fail:
+	fprintf(stderr, "WARN: failed to find tracker '%s' for removal.\n", tracker);
+	return 2;
 }
 
 int add1_fn(struct be_node *t, int argc, char **argv)
@@ -46,7 +138,9 @@ int add1_fn(struct be_node *t, int argc, char **argv)
 
 	struct be_node *a = be_find(t, "announce", n_tr);
 	if (!a) {
-		fprintf(stderr, "ADD: no mem\n");
+		free(tr);
+		free(n_tr);
+		fprintf(stderr, "ERR: %s\n", strerror(errno));
 		return -1;
 	} else if (a == n_tr) {
 		fprintf(stderr, "ADD: '%s' => 'announce'.\n", tracker);
@@ -57,11 +151,39 @@ int add1_fn(struct be_node *t, int argc, char **argv)
 
 	}
 
-	struct be_node *al = be_lookup(t, "announce-list");
-	if (!al) {
-		fprintf(stderr, "ADD: 'announce-list' not found.\n");
+	struct be_list *new_al = malloc(sizeof(*new_al));
+	new_al->len = 0;
+	new_al->nodes = 0;
+
+	struct be_node *new_al_n = malloc(sizeof(*new_al_n));
+	new_al_n->type = BE_LIST;
+	new_al_n->u.l = new_al;
+
+	struct be_node *al_n = be_find(t, "announce-list", new_al_n);
+	if (!al_n) {
+		fprintf(stderr, "ERR: %s\n", strerror(errno));
+		return -1;
+	} else if (al_n == new_al_n) {
+		fprintf(stderr, "ADD: created 'announce-list'.\n");
+	} else {
+		free(new_al);
+		free(new_al_n);
+		fprintf(stderr, "ADD: 'announce-list' already exsists.\n");
+	}
+
+	if (al_n->type != BE_LIST) {
+		fprintf(stderr, "ERR: 'announce-list' not a list.\n");
+
+		/* TODO: remove non list and add list on force tag? */
 		return -1;
 	}
+
+	struct be_list *al = al_n->u.l;
+
+	al->len ++;
+	al->nodes = realloc(al_nodes, al->len * sizeof(*al->nodes));
+
+	al->nodes[al->len - 1] = n_tr;
 
 	return 2;
 }
@@ -116,10 +238,14 @@ static int t_proc(struct be_node *t, int argc, char **argv)
 			ret = add1_fn(t, argc, argv);
 			break;
 		case 'r':
-			ret = rm_fn(t, argc, argv);
+			ret = del_fn(t, argc, argv);
 			break;
 		case 'p':
 			ret = t_show(t, argc, argv);
+			break;
+		default:
+			fprintf(stderr, "Bad option '%c'.\n", *cur_opt);
+			break;
 		}
 
 		if (ret > 0) {
@@ -129,7 +255,7 @@ static int t_proc(struct be_node *t, int argc, char **argv)
 			argc -= 1;
 			argv += 1;
 		} else { /* ret < 0 */
-			fprintf(stderr, "Bad option '%c'.\n", *cur_opt);
+			fprintf(stderr, "'%c' killed us.\n", *cur_opt);
 			return 1;
 		}
 	}
